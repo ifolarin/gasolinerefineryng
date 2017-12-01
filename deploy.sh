b09dfbeb2d74323bf9e468af1bf3d94ebc583a4f
Ibrahim Olukorede Folarin <ifolarin@webbasedsol.com>
2017-12-01 04:04:04 +0100

    customized deployment scripts for web only

---
+++ deploy.sh
@@ -0,0 +1,72 @@
+#! /bin/bash
+
+
+#Clone and build openldap
+#git clone https://github.com/ifolarin/docker-openldap.git
+#cd docker-openldap/example/extend-osixia-openldap/
+#docker build -t webbasedsol/openldap:0.3.4 .
+#docker tag  webbasedsol/openldap:0.3.4 webbasedsol/openldap:latest
+#cd -
+
+#Clone and build phpLDAPadmin
+#git clone https://github.com/ifolarin/docker-phpLDAPadmin.git
+#cd docker-phpLDAPadmin/example/extend-dinkel-phpldapadmin/
+#docker build -t webbasedsol/phpldapadmin:0.1.2 . 
+#docker tag  webbasedsol/phpldapadmin:0.1.2 webbasedsol/phpldapadmin:latest
+#cd -
+
+#Clone and build rainloop
+#git clone https://github.com/ifolarin/docker-rainloop.git
+#cd docker-rainloop/extend-runningman84-rainloop/
+#docker build -t webbasedsol/rainloop:0.2.0 .
+#docker tag  webbasedsol/rainloop:0.2.0 webbasedsol/rainloop:latest
+#cd -
+
+#Clone and build s3-backup
+#git clone https://github.com/ifolarin/docker-s3-cron-backup.git
+#cd docker-s3-cron-backup
+#docker build -t webbasedsol/s3-cron-backup:0.1.0
+#docker tag  webbasedsol/s3-cron-backup:0.1.0 webbasedsol/s3-cron-backup:latest
+#cd -
+
+
+#Clone and build backoffice application
+mkdir -p wp-data
+mkdir -p db-data
+mkdir -p config/letsencrypt/log
+mkdir -p config/letsencrypt/etc
+cd config/letsencrypt
+docker run --rm -ti -v $PWD/log/:/var/log/letsencrypt -v $PWD/etc/:/etc/letsencrypt/ -p 443:443 haocen/certbot:latest certonly --standalone \
+				-d gasolinerefineryng.com
+				-d www.gasolinerefineryng.com
+cd - 
+
+
+#Enable firewall
+echo -e "Enabling ufw firewall..."
+ufw enable
+
+#Allowing port traffic
+echo -e "Enabling ports..."
+
+echo -e "ssh..."
+ufw allow ssh
+echo -e "done."
+ 
+echo -e "http..."
+ufw allow http
+echo -e "done."
+
+echo -e "https..."
+ufw allow https
+echo -e "done."
+
+#echo -e "143..."
+#ufw allow 143
+#echo -e "done."
+#
+#echo -e "587..."
+#ufw allow 587
+#echo -e "done."
+
+
---
+++ setup-cert.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#docker pull haocen/certbot
+
+GetCert(){
+				docker run -it \
+								--rm \
+								-v $(pwd)/config/letsencrypt/etc:/etc/letsencrypt \
+								-v $(pwd)/config/letsencrypt/lib:/var/lib/letsencrypt \
+								-v $(pwd)/config/letsencrypt/www:/var/www/.well-known \
+							  haocen/certbot -t certonly --webroot -w /var/www \
+								--keep-until-expiring \
+								$@
+}
+
+echo "Getting certificates..."
+GetCert -d gasolinerefineryng.com -d www.gasolinerefineryng.com 
+
+
+
+
+
+								
